b981f6b8453347d609cedf55f70c5764940baf86
alexandradew <alexandradew@protonmail.com>
2020-05-13 00:33:51 -0300

    spelling fix

--- spaceship.js
+++ spaceship.js
@@ -1,229 +1,227 @@
-
-	// variables
-	var lifes = 3,
+// variables
+var lives = 3,
 	points = 0,
 	meteorMOVE = false,
 	fireSPEED = 8,
 	speed = 8,
 	gameOver = false,
-	shipLEFT = false, 
+	shipLEFT = false,
 	shipRIGHT = false,
 	fireUP = false,
 	LEFT = 37,
 	RIGHT = 39
 
-	// images
-	background = new Image();
-	background.src = "img/bg.jpg";
+// images
+background = new Image();
+background.src = "img/bg.jpg";
 
-	var shipIMG = new Image();
-	shipIMG.src = "img/ship.png";
+var shipIMG = new Image();
+shipIMG.src = "img/ship.png";
 
-	var meteorIMG = new Image();
-	meteorIMG.src = "img/meteor.png";
+var meteorIMG = new Image();
+meteorIMG.src = "img/meteor.png";
 
-	var bulletIMG = new Image();
-	bulletIMG.src = "img/bullet.png";
+var bulletIMG = new Image();
+bulletIMG.src = "img/bullet.png";
 
-	var cnv = document.querySelector('canvas');
-	var ctx = cnv.getContext('2d');
+var cnv = document.querySelector('canvas');
+var ctx = cnv.getContext('2d');
 
-	// objects
-	var ship = {
-		x:270,
-		y:670,
-		w:80,
-		h:100
-	}
+// objects
+var ship = {
+	x: 270,
+	y: 670,
+	w: 80,
+	h: 100
+}
 
-	bullets = [];
+bullets = [];
 
-	meteors=[
-	{
-		x:Math.floor(Math.random() * 520) + 1,
-		y:-70,
-		w:40,
-		h:60
+meteors = [{
+		x: Math.floor(Math.random() * 520) + 1,
+		y: -70,
+		w: 40,
+		h: 60
 	},
 	{
-		x:Math.floor(Math.random() * 520) + 1,
-		y:-60,
-		w:40,
-		h:60
+		x: Math.floor(Math.random() * 520) + 1,
+		y: -60,
+		w: 40,
+		h: 60
 	},
 	{
-		x:Math.floor(Math.random() * 520) + 1,
-		y:-90,
-		w:40,
-		h:60
+		x: Math.floor(Math.random() * 520) + 1,
+		y: -90,
+		w: 40,
+		h: 60
 	}
-	];
+];
 
-		
-	// listeners
 
-	window.addEventListener("keydown", function keyDown(e){
-		var key =	e.keyCode;
-		if (key === LEFT && key !== RIGHT){
-			shipLEFT = true;
-			shipRIGHT = false;
-		}
+// listeners
 
-		if (key === RIGHT && key !== LEFT){
-			shipRIGHT = true;
-			shipLEFT = false;
-		}
+window.addEventListener("keydown", function keyDown(e) {
+	var key = e.keyCode;
+	if (key === LEFT && key !== RIGHT) {
+		shipLEFT = true;
+		shipRIGHT = false;
+	}
 
-		if (key === 65){
-			side = Math.floor(Math.random() * 2) + 1
-			if (side == 1){
-				bullets.push({
-					x:ship.x+17,
-					y:ship.y+35,
-					w:5,
-					h:10
-				})
-			}
-			if (side == 2){
-				bullets.push({
-					x:ship.x+55,
-					y:ship.y+35,
-					w:5,
-					h:10
-				})
-			}
-			fireUP = true
-		}
-	});
+	if (key === RIGHT && key !== LEFT) {
+		shipRIGHT = true;
+		shipLEFT = false;
+	}
 
-	window.addEventListener("keyup", function keyUp(e){
-		var key =	e.keyCode;
-		if (key == LEFT && key !== RIGHT){
-			shipLEFT = false;
+	if (key === 65) {
+		side = Math.floor(Math.random() * 2) + 1
+		if (side == 1) {
+			bullets.push({
+				x: ship.x + 17,
+				y: ship.y + 35,
+				w: 5,
+				h: 10
+			})
 		}
-		if (key == RIGHT && key !== LEFT){
-			shipRIGHT = false;
+		if (side == 2) {
+			bullets.push({
+				x: ship.x + 55,
+				y: ship.y + 35,
+				w: 5,
+				h: 10
+			})
 		}
-	});
+		fireUP = true
+	}
+});
+
+window.addEventListener("keyup", function keyUp(e) {
+	var key = e.keyCode;
+	if (key == LEFT && key !== RIGHT) {
+		shipLEFT = false;
+	}
+	if (key == RIGHT && key !== LEFT) {
+		shipRIGHT = false;
+	}
+});
 
 // movimentação
 
-	function shipAction(){
-		if(shipLEFT){
-			ship.x-=speed;
-		}
-
-		if(shipRIGHT){
-			ship.x+=speed;
-		}
-
-		if(ship.x > 520){
-			ship.x = 520;
-		}
-		if(ship.x < 0){
-			ship.x = 0;
-		}
-
-		if (fireUP){
-			bullets.forEach(function(value,i,array){
-				bullets[i].y-=4;
-			})
-		}
+function shipAction() {
+	if (shipLEFT) {
+		ship.x -= speed;
 	}
 
-	function collision(){
-		bullets.forEach(function(value,i,array){
-			meteors.forEach(function(value,m,array){
-				if(bullets[i].y >= meteors[m].y && bullets[i].y <= meteors[m].y+meteors[m].h && bullets[i].x >= meteors[m].x && bullets[i].x <= meteors[m].x+meteors[m].w){
-					meteors[m].x = Math.floor(Math.random() * 520) + 1;
-					meteors[m].y = -30;
-					delete bullets[i];
-					points+=1
-			}
-			})
+	if (shipRIGHT) {
+		ship.x += speed;
+	}
+
+	if (ship.x > 520) {
+		ship.x = 520;
+	}
+	if (ship.x < 0) {
+		ship.x = 0;
+	}
+
+	if (fireUP) {
+		bullets.forEach(function (value, i, array) {
+			bullets[i].y -= 4;
 		})
 	}
+}
 
-	function clearBullets(){
-		bullets.forEach(function(value,i,array){
-			if (bullets[i].y < 0){
+function collision() {
+	bullets.forEach(function (value, i, array) {
+		meteors.forEach(function (value, m, array) {
+			if (bullets[i].y >= meteors[m].y && bullets[i].y <= meteors[m].y + meteors[m].h && bullets[i].x >= meteors[m].x && bullets[i].x <= meteors[m].x + meteors[m].w) {
+				meteors[m].x = Math.floor(Math.random() * 520) + 1;
+				meteors[m].y = -30;
 				delete bullets[i];
+				points += 1
 			}
 		})
-	}
+	})
+}
 
-	function checkStatus(){
-		if (meteorMOVE){
-			meteors.forEach(function(value,i,array){
-				meteors[i].y+=2;
-			})
+function clearBullets() {
+	bullets.forEach(function (value, i, array) {
+		if (bullets[i].y < 0) {
+			delete bullets[i];
 		}
-		if (lifes <= 0){
-			gameOver = true;
-		}
-	}
+	})
+}
 
-
-
-	function render(){
-		ctx.clearRect(0,0,cnv.width,cnv.height);
-		ctx.drawImage(background,0,0);
-		//text information
-		ctx.font = "20px Arial";
-		ctx.fillText("SCORE: "+ points, 20, 790);
-		ctx.fillText("LIFES: "+ lifes,450, 790);
-		ctx.fillStyle = "rgb(236, 240, 241)";
-		//ship
-		ctx.drawImage(shipIMG,ship.x, ship.y,ship.w,ship.h);
-		//meteors
-		meteors.forEach(function(value,i,array){
-			ctx.drawImage(meteorIMG,meteors[i].x, meteors[i].y, meteors[i].w,meteors[i].h);
-			if (meteors[i].y > 800 ){
-				meteors[i].x = Math.floor(Math.random() * 520) + 1;
-				meteors[i].y = -30;
-				lifes-=1;
-			}
+function checkStatus() {
+	if (meteorMOVE) {
+		meteors.forEach(function (value, i, array) {
+			meteors[i].y += 2;
 		})
-		// bullets
-		bullets.forEach(function(value,i,array){
-			ctx.drawImage(bulletIMG,bullets[i].x, bullets[i].y, bullets[i].w,bullets[i].h);
-		})
+	}
+	if (lives <= 0) {
+		gameOver = true;
+	}
+}
 
-		if(gameOver){
-			bullets = [];
-			ctx.clearRect(0,0,cnv.width,cnv.height);
-			ctx.drawImage(background,0,0);
-			ctx.font = "25px Arial";
-			ctx.fillText("GAME OVER", 225, 320);
-			ctx.font = "15px Arial";
-			ctx.fillText("You score "+ points, 230, 350);
-			document.getElementById('restart').style.display = 'block';
 
+function render() {
+	ctx.clearRect(0, 0, cnv.width, cnv.height);
+	ctx.drawImage(background, 0, 0);
+	//text information
+	ctx.font = "20px Arial";
+	ctx.fillText("SCORE: " + points, 20, 790);
+	ctx.fillText("LIVES: " + lives, 450, 790);
+	ctx.fillStyle = "rgb(236, 240, 241)";
+	//ship
+	ctx.drawImage(shipIMG, ship.x, ship.y, ship.w, ship.h);
+	//meteors
+	meteors.forEach(function (value, i, array) {
+		ctx.drawImage(meteorIMG, meteors[i].x, meteors[i].y, meteors[i].w, meteors[i].h);
+		if (meteors[i].y > 800) {
+			meteors[i].x = Math.floor(Math.random() * 520) + 1;
+			meteors[i].y = -30;
+			lives -= 1;
 		}
-	}
+	})
+	// bullets
+	bullets.forEach(function (value, i, array) {
+		ctx.drawImage(bulletIMG, bullets[i].x, bullets[i].y, bullets[i].w, bullets[i].h);
+	})
 
-	function start(){
-		document.getElementById('start').style.display = 'none';
-		meteorMOVE = true;
-	}
+	if (gameOver) {
+		bullets = [];
+		ctx.clearRect(0, 0, cnv.width, cnv.height);
+		ctx.drawImage(background, 0, 0);
+		ctx.font = "25px Arial";
+		ctx.fillText("GAME OVER", 225, 320);
+		ctx.font = "15px Arial";
+		ctx.fillText("Your score " + points, 230, 350);
+		document.getElementById('restart').style.display = 'block';
 
-	function restart(){
-		document.getElementById('restart').style.display = 'none';
-		meteors.forEach(function(value,i,array){
-				meteors[i].y= -70;
-			})
-		gameOver = false ;
-		lifes = 3;
-		meteorMOVE = true;
-		points = 0;
 	}
+}
 
-	main();
-	function main(){ 
-		requestAnimationFrame(main, cnv);
-		shipAction();
-		collision();
-		checkStatus();
-		clearBullets();
-		render();
-	}
+function start() {
+	document.getElementById('start').style.display = 'none';
+	meteorMOVE = true;
+}
+
+function restart() {
+	document.getElementById('restart').style.display = 'none';
+	meteors.forEach(function (value, i, array) {
+		meteors[i].y = -70;
+	})
+	gameOver = false;
+	lives = 3;
+	meteorMOVE = true;
+	points = 0;
+}
+
+main();
+
+function main() {
+	requestAnimationFrame(main, cnv);
+	shipAction();
+	collision();
+	checkStatus();
+	clearBullets();
+	render();
+}
